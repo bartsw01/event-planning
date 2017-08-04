7b10421b6b5e93e88e1cd919b503369749e86212
Bart Webber <bart@aseenrichment.com>
2017-08-04 10:55:58 -0700

    2nd test passed - 3rd test failed mixed case WeDdingG to lowercase wedding

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 public class EventTest {
 
     @Test
-    public void eventOptions_shouldBeASting_true() throws Exception {
+    public void eventOptions_shouldBeAString_true() throws Exception {
         Event testOption = new Event();
         String userOption = "wedding";
         assertEquals(userOption, testOption.runOption("wedding"));
@@ -16,5 +16,13 @@ public class EventTest {
         Event testOption = new Event();
         String userOption = "birthday";
         assertEquals(userOption, testOption.runOption("birthday"));
+    }
+
+    @Test
+    public void eventOptions_mixedCaseToLowerCase_true() throws Exception {
+        Event testOption = new Event();
+        String userOption = "WeDdinG";
+        assertEquals(userOption, testOption.runOption("wedding"));
+
     }
 }
