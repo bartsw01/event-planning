9006f2503b519a30fdb298062f0815ce8fb6a77d
Bart Webber <bart@aseenrichment.com>
2017-08-04 10:43:00 -0700

    add 2nd test bithday input string return birthday - failed

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,7 +1,8 @@
 public class Event {
     public String wedding;
     public String corporate;
-    public String party;
+    public String birthday;
+    public String reunion;
     public int numberOfGuests;
 
     public String runOption(String inputString){
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -10,4 +10,11 @@ public class EventTest {
         String userOption = "wedding";
         assertEquals(userOption, testOption.runOption("wedding"));
     }
+
+    @Test
+    public void eventOptions_shouldBeAString2_true() throws Exception {
+        Event testOption = new Event();
+        String userOption = "birthday";
+        assertEquals(userOption, testOption.runOption("birthday"));
+    }
 }
