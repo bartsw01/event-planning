188252a4ea0a4ffe65a3e1dee79967e81e9a4609
Bart Webber <bart@aseenrichment.com>
2017-08-04 10:33:21 -0700

    add first test to return wedding option string

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,10 @@
 public class Event {
+    public String wedding;
+    public String corporate;
+    public String party;
+    public int numberOfGuests;
+
+    public String runOption(String inputString){
+        return inputString;
+    }
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -1,5 +1,13 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EventTest {
 
+    @Test
+    public void eventOptions_shouldBeASting_true() throws Exception {
+        Event testOption = new Event();
+        String userOption = "wedding";
+        assertEquals(userOption, testOption.runOption("wedding"));
+    }
 }
