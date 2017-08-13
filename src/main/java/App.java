55e9da6471fa2c1c1d9aa89ce3789406a3dd883c
Bart Webber <bart@aseenrichment.com>
2017-08-13 14:36:03 -0700

    add coupon code logic to App.java frontend

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,7 +22,21 @@ public class App {
             Event newEvent = new Event(packageType, numOfGuests, food, beverage, entertainment);
             int totalCost = newEvent.getEventCost();
 
+            if (numOfGuests >= 250 && numOfGuests <= 499){
+                System.out.println("Based on the number of your guests, you are eligible for a discount. Please use discount code: guest250");
+            } else if (numOfGuests >= 500 && numOfGuests <= 999){
+                System.out.println("Based on the number of your guests, you are eligible for a discount. Please use discount code: guest500");
+            } else if (numOfGuests >= 1000){
+                System.out.println("Based on the number of your guests, you are eligible for a discount. Please use discount code: guest1000");
+            }
+
             System.out.println("The total cost of your event will be $" + totalCost + ".");
+
+            if (numOfGuests >= 250){
+                System.out.println("Please enter your discount code to apply your discount to the total cost of your event");
+                String discountCode = bufferedReader.readLine();
+                System.out.println("This is the updated cost of your event after applying your discount. $"  + newEvent.getDiscountedEventCost(discountCode));
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
