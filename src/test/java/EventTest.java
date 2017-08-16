91ca5b6f3a628a53dea10c60ad41c3a61502ec79
Bart Webber <bart@aseenrichment.com>
2017-08-16 08:32:20 -0700

    add higher cost of DJ to 6th test, which now passes

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -43,7 +43,7 @@ public class EventTest {
     @Test
     public void calculateEventCost_returnsCostForReunion_23400() throws Exception {
         Event testEvent = new Event("reunion", 500, "chicken dinner", "cash bar", "dj");
-        Integer expected = 23400;
+        Integer expected = 23900;
         assertEquals(expected, testEvent.getEventCost());
     }
 
